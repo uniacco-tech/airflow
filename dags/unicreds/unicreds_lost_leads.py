b8b6c059c5767df806c0f92dbaf3ed648e6bf11c
nishantsharma009 <151528405+nishantsharma009@users.noreply.github.com>
2025-12-12 17:45:09 +0530

    Create unicreds_lost_leads.py

---
+++ dags/unicreds/unicreds_lost_leads.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import sys
+import os
+
+SCRIPTS_PATH = '/git/current/script/unischolars'
+sys.path.append(SCRIPTS_PATH)
+
+from uc_lost_lead_dump_report import run_lost_lead_dump_etl
+
+default_args = {
+    "owner": "nishant",
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="unicreds_lost_leads", 
+    default_args=default_args,
+    description="Generates daily UniCreds, UniScholars, and UniAcco lost lead data dumps for key lost reasons and opportunity leads.",
+    schedule_interval="30 3 */2 * *",
+    start_date=datetime(2025, 12, 12),
+    max_active_runs=1,
+    catchup=False,
+    tags=["UniCreds", "Email"]
+) as dag:
+
+    lost_lead_dump_task = PythonOperator(
+        task_id="generate_and_send_lost_lead_dump", 
+        python_callable=run_lost_lead_dump_etl
+    )
+
+    lost_lead_dump_task
